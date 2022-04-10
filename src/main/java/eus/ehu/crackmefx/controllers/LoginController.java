cb0e90f1739da1b27415d3634a467ea734d95b0e
Juanan Pereira <juanan@ikasten.io>
2022-04-10 22:55:40 +0200

    Comment out Logging support

--- src/main/java/eus/ehu/crackmefx/controllers/LoginController.java
+++ src/main/java/eus/ehu/crackmefx/controllers/LoginController.java
@@ -9,8 +9,8 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.concurrent.atomic.AtomicBoolean;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
+// import org.apache.logging.log4j.Logger;
+// import org.apache.logging.log4j.LogManager;
 
 /*
         logger.info("Info log message");
@@ -20,7 +20,7 @@ import org.apache.logging.log4j.LogManager;
 
 public class LoginController {
 
-        private Logger logger;
+        // private Logger logger;
 
         @FXML
         private TextField serial;
@@ -93,7 +93,6 @@ public class LoginController {
         }
 
         private boolean rule1(String serial) {
-                logger.debug("The length of the serial should be 19");
                 return serial.length() == 19;
         }
 
@@ -101,7 +100,8 @@ public class LoginController {
 
         @FXML
         void initialize() {
-                logger = LogManager.getLogger(LoginController.class);
+                // logger = LogManager.getLogger(LoginController.class);
+                // logger.debug("Starting the crackme...");
         }
 
         public void setMain(Crackme crackme) {
